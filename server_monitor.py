ba10d14a765e1a2b3f96c3797dcc4631240ea03c
Byenna <72694655+Byenna@users.noreply.github.com>
2023-08-14 22:00:55 +0200

    created seperate file for visualizations

--- server_monitor.py
+++ server_monitor.py
@@ -13,8 +13,7 @@ def index():
     # Add code here to retrieve server performance data using psutil
     # Store the data in suitable variables to pass to the template
     # Example data for demonstration purposes
-    cpu_percents = psutil.cpu_percent(interval=1, percpu=True)
-    cores = list(range(len(cpu_percents)))
+    
     start_time = time.time()  # Record the start time
     cpu_usage = psutil.cpu_percent(interval=None)
     memory = psutil.virtual_memory()
@@ -34,7 +33,23 @@ def index():
     end_time = time.time()  # Record the end time
     response_time = end_time - start_time  # Calculate response time in seconds
 
-    # Memory usage
+  
+
+    return render_template('dashboard_template.html',
+                            cpu_usage=cpu_usage,
+                           total_memory=total_memory, available_memory=available_memory,
+                           memory_usage=memory_usage, total_disk_space=total_disk_space,
+                           used_disk_space=used_disk_space, free_disk_space=free_disk_space,
+                           disk_space_usage=disk_space_usage, bytes_sent=bytes_sent,
+                           bytes_received=bytes_received, packets_sent=packets_sent,
+                           packets_received=packets_received, response_time=response_time)
+
+@app.route('/visualizations')
+def visualizations():
+    cpu_percents = psutil.cpu_percent(interval=1, percpu=True)
+    cores = list(range(len(cpu_percents)))
+
+      # Memory usage
     memory = psutil.virtual_memory()
     memory_used = memory.used / (1024 ** 2)  # Convert to MB
     memory_available = memory.available / (1024 ** 2)  # Convert to MB
@@ -44,17 +59,12 @@ def index():
 
     # CPU usage
     cpu_plot_div = create_cpu_plot(cores, cpu_percents)
-
-    return render_template('dashboard_template.html',
+    # Add code here to retrieve and pass data for visualizations if needed
+    return render_template('visualizations.html',
                            memory_plot_div=memory_plot_div,
                            cpu_plot_div=cpu_plot_div,
-                           memory_percent=memory_percent, cpu_usage=cpu_usage,
-                           total_memory=total_memory, available_memory=available_memory,
-                           memory_usage=memory_usage, total_disk_space=total_disk_space,
-                           used_disk_space=used_disk_space, free_disk_space=free_disk_space,
-                           disk_space_usage=disk_space_usage, bytes_sent=bytes_sent,
-                           bytes_received=bytes_received, packets_sent=packets_sent,
-                           packets_received=packets_received, response_time=response_time)
+                           memory_percent=memory_percent)
+
 
 def create_memory_plot(used, available):
     data = go.Bar(x=['Used', 'Available'], y=[used, available])
