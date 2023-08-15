4706fd81579366901a5735ce8a35f76a7ec456f4
Byenna <72694655+Byenna@users.noreply.github.com>
2023-08-15 12:52:25 +0200

    created venv and installing modules

--- server_monitor.py
+++ server_monitor.py
@@ -3,8 +3,23 @@ import flask
 import time
 import plotly.graph_objs as go
 import plotly.offline
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from config import EMAIL_ADDRESS, EMAIL_PASSWORD
+import requests
 
-from flask import Flask, render_template
+# Define the URL of your Flask app
+url = 'http://localhost:5000/check_metrics'  # Replace with the actual URL of your app
+
+# Send a POST request
+response = requests.post(url)
+
+# Print the response
+print(response.text)
+
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='templates')
 
@@ -36,7 +51,7 @@ def index():
   
 
     return render_template('dashboard_template.html',
-                            cpu_usage=cpu_usage,
+                           cpu_usage=cpu_usage,
                            total_memory=total_memory, available_memory=available_memory,
                            memory_usage=memory_usage, total_disk_space=total_disk_space,
                            used_disk_space=used_disk_space, free_disk_space=free_disk_space,
@@ -65,7 +80,6 @@ def visualizations():
                            cpu_plot_div=cpu_plot_div,
                            memory_percent=memory_percent)
 
-
 def create_memory_plot(used, available):
     data = go.Bar(x=['Used', 'Available'], y=[used, available])
     layout = go.Layout(title='Memory Utilization', xaxis={'title': 'Memory Type'}, yaxis={'title': 'Memory (MB)'})
@@ -79,6 +93,30 @@ def create_cpu_plot(cores, cpu_percents):
     fig = go.Figure(data=[data], layout=layout)
     plot_div = plotly.offline.plot(fig, output_type='div', show_link=False)
     return plot_div
+
+@app.route('/check_metrics', methods=['POST'])
+def check_metrics():
+    cpu_usage = get_cpu_usage()  # Implement a function to retrieve CPU usage
+
+    if cpu_usage > 90:  # Your threshold value
+        subject = 'Alert: High CPU Usage'
+        message = f'CPU usage is {cpu_usage}%, which is above the threshold.'
+        send_email_alert(subject, message)
+
+    return 'Metrics checked'
+
+def send_email_alert(subject, message):
+    msg = MIMEMultipart()
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = 'recipient@example.com'
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'plain'))
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        server.send_message(msg)
+
 
 if __name__ == '__main__':
     app.run(debug=True)  # debug=True for development mode
